10b171f55e767363b0ccf60bcdb808c99a657d2b
adam0306 <AdamChandler0306@gmail.com>
2022-07-01 21:05:50 -0500

    Can we build our own website with database in GO?

---
+++ website/website.go
@@ -0,0 +1,57 @@
+http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
+})
+
+
+func (w http.ResponseWriter, r *http.Request)
+
+
+http.ListenAndServe(":80", nil)
+
+
+
+package main
+
+import (
+    "fmt"
+    "net/http"
+)
+
+func main() {
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+        fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
+    })
+
+    http.ListenAndServe(":80", nil)
+}
+
+
+
+// passwords.go
+package main
+
+import (
+    "fmt"
+    "golang.org/x/crypto/bcrypt"
+)
+
+func HashPassword(password string) (string, error) {
+    bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+    return string(bytes), err
+}
+
+func CheckPasswordHash(password, hash string) bool {
+    err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+    return err == nil
+}
+
+func main() {
+    password := "secret"
+    hash, _ := HashPassword(password) // ignore error for the sake of simplicity
+
+    fmt.Println("Password:", password)
+    fmt.Println("Hash:    ", hash)
+
+    match := CheckPasswordHash(password, hash)
+    fmt.Println("Match:   ", match)
+}

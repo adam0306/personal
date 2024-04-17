640fdd78b4cb3fbc3dba1ecf9cdca93d600b18b9
adam0306 <AdamChandler0306@gmail.com>
2024-04-17 16:45:46 -0500

    Shell script for CP call

---
+++ cyberark/shell/cp.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Set Path to directory of SDK.
+#export PATH=$PATH:/opt/CARKaim/sdk
+# Store user input in variables.
+# app = $1
+# username = $2
+# safe = $3
+
+echo $1
+echo $2
+echo $3
+#, $username, $safe
+
+
+# address and safe passed at running of exe
+# set path
+# static username
+
+
+# ./clipasswordsdk GetPassword -p AppDescs.AppID=<$app> -p Query="Safe=<$safe>;Object=<object-name>" -p RequiredProps=Address,UserName -o Password,PassProps.UserName 
+
+
+
+# test = read -p
+
+# echo "test: test"
+
+# echo "First Second Third Fourth " | while read -r -d ' ' ret1; do printf "=$ret1= \n"; done
+# =First=
+# =Second=
+# =Third=
+# =Fourth=
+
+
+
+# 

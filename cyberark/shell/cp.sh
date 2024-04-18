faaf21f280d5cfefe6354c2bc4f03a24c850eda7
adam0306 <AdamChandler0306@gmail.com>
2024-04-17 22:31:16 -0500

    getting there

--- cyberark/shell/cp.sh
+++ cyberark/shell/cp.sh
@@ -7,31 +7,14 @@
 # username = $2
 # safe = $3
 
-echo $1
-echo $2
-echo $3
-#, $username, $safe
+$app = $1
+$safe = $2
+$obj = $3
 
+#cd /opt/CARKaim/sdk/
 
-# address and safe passed at running of exe
-# set path
-# static username
+$result = "./opt/CARKaim/sdk/clipasswordsdk GetPassword -p AppDescs.AppID=<$app> -p Query="Safe=<$safe>;Object=<$obj>" -p RequiredProps=Address,UserName -o Password,PassProps.UserName"
 
-
-# ./clipasswordsdk GetPassword -p AppDescs.AppID=<$app> -p Query="Safe=<$safe>;Object=<object-name>" -p RequiredProps=Address,UserName -o Password,PassProps.UserName 
-
-
-
-# test = read -p
-
-# echo "test: test"
-
-# echo "First Second Third Fourth " | while read -r -d ' ' ret1; do printf "=$ret1= \n"; done
-# =First=
-# =Second=
-# =Third=
-# =Fourth=
-
-
-
-# 
+# Print username and password to terminal.
+echo $result.UserName
+echo $result.Password

ae400eadc2b16afcbb61068c7b7488071baa07fa
adam0306 <AdamChandler0306@gmail.com>
2024-04-18 08:50:36 -0500

    rough draft

--- cyberark/shell/cp.sh
+++ cyberark/shell/cp.sh
@@ -1,19 +1,27 @@
 #!/bin/bash
 
-# Set Path to directory of SDK.
-#export PATH=$PATH:/opt/CARKaim/sdk
-# Store user input in variables.
-# app = $1
-# username = $2
-# safe = $3
+#Create Help information.
+display_help() {
+    echo "Usage: $0 [option...] {start|stop|restart}" >&2
+    echo "   -r, --resolution           run with the given resolution WxH"
+    echo "   -d, --display              Set on which display to host on "
+    # echo some stuff here for the -a or --add-options 
+    exit 1
+}
 
+# Converting provided input to $app variable.
 $app = $1
+# Converting provided input to $safe variable.
 $safe = $2
+# Converting provided input to $obj variable.
 $obj = $3
 
-#cd /opt/CARKaim/sdk/
+# Execute with path
+#$result = "./opt/CARKaim/sdk/clipasswordsdk GetPassword -p AppDescs.AppID=<$app> -p Query="Safe=<$safe>;Object=<$obj>" -p RequiredProps=Address,UserName -o Password,PassProps.UserName"
 
-$result = "./opt/CARKaim/sdk/clipasswordsdk GetPassword -p AppDescs.AppID=<$app> -p Query="Safe=<$safe>;Object=<$obj>" -p RequiredProps=Address,UserName -o Password,PassProps.UserName"
+# Execute with path
+cd /opt/CARKaim/sdk/
+$result = "./clipasswordsdk GetPassword -p AppDescs.AppID=<$app> -p Query="Safe=<$safe>;Object=<$obj>" -p RequiredProps=Address,UserName -o Password,PassProps.UserName"
 
 # Print username and password to terminal.
 echo $result.UserName
